ddad1d71ce209872fcec4aaed6a84e42f0ee43d3
Decon123 <87056906+Decon123@users.noreply.github.com>
2022-02-12 12:38:25 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+/*let firstName = "Decon";
+let lastName = "Kelaart";
+//string concatenation
+let fullName = firstName + " " + lastName;
+console.log(fullName);
+//length of a string
+console.log(fullName.length);*/
+
+let cardDescribe = document.querySelector(".card-describe").textContent;
+console.log(cardDescribe.length);
